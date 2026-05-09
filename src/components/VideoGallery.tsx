1f7526f4fe72a166572284577f91f26585760454
wiro34 <too@wirog.net>
2026-05-09 11:21:16 +0900

    fixes archives' thumb resolution

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -4,14 +4,19 @@ import { VideoCard } from "./VideoCard";
 interface VideoGalleryProps {
   videos: Video[];
   onVideoClick: (video: Video) => void;
+  thumbnailSize?: "default" | "mqdefault" | "hqdefault" | "sddefault" | "maxresdefault";
 }
 
-export function VideoGallery({ videos, onVideoClick }: VideoGalleryProps) {
+export function VideoGallery({
+  videos,
+  onVideoClick,
+  thumbnailSize = "hqdefault",
+}: VideoGalleryProps) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4">
       {videos.map((video) => (
         <div key={video.video_id}>
-          <VideoCard video={video} onClick={onVideoClick} />
+          <VideoCard video={video} onClick={onVideoClick} thumbnailSize={thumbnailSize} />
         </div>
       ))}
     </div>
